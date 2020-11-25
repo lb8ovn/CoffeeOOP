73f16b6745f97136aa17f78cd96b4d89b471e6b1
Joel Holley <joel.holley@gmail.com>
2020-11-25 15:22:15 +0000

    Fixed grammar

--- Day1.py
+++ Day1.py
@@ -1,7 +1,7 @@
 #Let's do this
 print('What is the name of the city you grew up in?')
 x = input()
-print('pet name?')
+print('What is your pet name?')
 y = input()
 print(str(x) + ' ' + str(y))
 print('Press enter')
