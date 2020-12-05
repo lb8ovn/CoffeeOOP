8c160d2b1d2f1ad3afbc27a363772c4b93777cf6
Joel Holley <joel.holley@gmail.com>
2020-12-05 11:56:12 +0000

    Fixed names

---
+++ PrimeChecker.py
@@ -0,0 +1,9 @@
+def prime(number):
+    y = False
+    for x in range(2,number):
+        if number%x == 0:
+            y = True
+    if y == True:
+        print('Not a Prime')
+    else:
+        print('Prime')
