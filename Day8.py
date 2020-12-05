b96fed90ed0a4198f9f156943100f0e2582a9261
Joel Holley <joel.holley@gmail.com>
2020-12-05 12:19:57 +0000

    First Step

--- Day8.py
+++ Day8.py
@@ -1,3 +1,22 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
+def encrypt(text, shift):
+    un_word = list(text)
+    en_word = ''
+    for letter in un_word:
+        position = alphabet.index(letter)
+        en_pos = position + shift
+        if en_pos < 25:
+            en_letter = str(alphabet[en_pos])
+            en_word += en_letter
+        else:
+            en_pos = en_pos-26
+            en_letter = str(alphabet[en_pos])
+            en_word += en_letter
+    print(en_word)
 
+encrypt(text, shift)
