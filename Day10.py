9920a992233f32e58f806e3565646468ef19ce28
Joel Holley <joel.holley@gmail.com>
2020-12-11 10:39:21 +0000

    Day 10

--- Day10.py
+++ Day10.py
@@ -9,13 +9,17 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
-
-math = {"+":add,"-":subtract,"*":multiply,"/":divide}
-num1 = int(input("First number "))
-num2 = int(input("Second number "))
-for x in math:
-    print(x)
-operation = input("Symbol ")
-calculation = math[operation]
-answer = calculation(num1,num2)
-print(f"{num1} {operation} {num2} = {answer}")
+y= True
+while y == True:
+    math = {"+":add,"-":subtract,"*":multiply,"/":divide}
+    num1 = int(input("First number "))
+    num2 = int(input("Second number "))
+    for x in math:
+        print(x)
+    operation = input("Symbol ")
+    calculation = math[operation]
+    answer = calculation(num1,num2)
+    print(f"{num1} {operation} {num2} = {answer}")
+    z = input("Wanna keep going? (y/n)")
+    if z == "n":
+        y = False
